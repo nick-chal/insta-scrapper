fefec811eff7f368c8d2fad858ea4f2e7eb84573
Nischal Shrestha <er.nischalshrestha@gmail.com>
2020-07-21 20:45:13 +0545

    add env for creds

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+import dotenv from 'dotenv'
 import { IgApiClient } from "instagram-private-api";
-import { sample } from "lodash";
+
+dotenv.config()
+
+const username = process.env.username
+const password = process.env.password
+
 const ig = new IgApiClient();
 // You must generate device id's before login.
 // Id's generated based on seed
 // So if you pass the same value as first argument - the same id's are generated every time
 (async () => {
   const ig = new IgApiClient();
-  ig.state.generateDevice("wasas.mar");
+  ig.state.generateDevice(username);
   // ig.state.proxyUrl = process.env.IG_PROXY;
-  const auth = await ig.account.login("wasas.mar", "testpw123");
+  const auth = await ig.account.login(username, password);
   const user = await ig.user.getIdByUsername("thug_life_nepal_oficial");
   const data = await ig.user.info(user);
   console.log({
